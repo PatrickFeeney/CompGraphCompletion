3089c8a76ab2dca9d683787ec058c6cb2e8c3de3
PatrickFeeney <patrickfeeney1225@gmail.com>
2025-03-13 14:03:32 -0400

    SVI Graph Completion

--- comp_graph_completion.py
+++ comp_graph_completion.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+import pickle
+
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.gaussian_process import GaussianProcessRegressor
@@ -34,12 +37,17 @@ def vis_linspace(X: np.ndarray):
     return np.linspace(X.min() - X_range * 0.1, X.max() + X_range * 0.1, 200)
 
 
-def save_gp(gp: GaussianProcessRegressor):
-    pass
+def gp_fname(dataset1, col_name1, dataset2, col_name2):
+    return f"{dataset1}_{col_name1}_{dataset2}_{col_name2}.dump"
 
 
-def load_gp():
-    pass
+def save_gp(gp: GaussianProcessRegressor, fname: str):
+    with open(Path("./models") / Path(fname), "wb") as f:
+        pickle.dump(gp, f)
+
+
+def load_gp(fname: str):
+    return pickle.load(open(Path("./models") / Path(fname), "rb"))
 
 
 def vis_gp(X: np.ndarray, y, xlabel, ylabel, X_lin, mean_pred, std_pred):
@@ -62,18 +70,25 @@ def vis_gp(X: np.ndarray, y, xlabel, ylabel, X_lin, mean_pred, std_pred):
 
 if __name__ == "__main__":
     df = load_svi()
-    X = np.expand_dims(df["Totpop2000"].to_numpy(), axis=1)
-    X_lin = vis_linspace(X)
-    for col_name in ["G1V1R", "G1V2R", "G1V3R", "G1V4R"]:
+    dataset1 = "svi"
+    col_name1 = "minority"
+    X = np.expand_dims(df[col_name1].to_numpy(), axis=1)
+    for col_name in ["pov", "pov150"]:
+        dataset2 = "svi"
+        col_name2 = col_name
         y = df[col_name].to_numpy()
         # For each edge of interest, grab subset of rows such that each side of the edge has data
+        mask = np.logical_not(np.logical_or(np.isnan(X)[:, 0], np.isnan(y)))
+        mask_X = X[mask, :]
+        mask_y = y[mask]
         # Reshape data so that each row indexes a unique combo of location and time
         # Divide data into train and test based on time
         # Fit GP on the subset (grid search with validation set for kernel hyperparameters?)
-        gp = fit_gp(X, y)
+        gp = fit_gp(mask_X, mask_y)
         # or load the GP from a previous run
         # Save predictions and pickle model
+        X_lin = vis_linspace(mask_X)
         mean_pred, std_pred = pred_gp(X_lin[:, None], gp)
-        save_gp(gp)
+        save_gp(gp, gp_fname(dataset1, col_name1, dataset2, col_name2))
         # Visualize GP
-        vis_gp(X, y, "TotalPopulation", col_name, X_lin, mean_pred, std_pred)
+        vis_gp(mask_X, mask_y, "Minority %", col_name, X_lin, mean_pred, std_pred)
